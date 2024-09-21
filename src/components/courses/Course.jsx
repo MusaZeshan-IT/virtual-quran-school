ba849436a1bbb1a991c905578ce16c4bb8cc5fcf
Musa Zeshan <musazeshan.it@gmail.com>
2024-09-21 20:31:19 +0500

    Changed the color to amber from emerald on the homepage courses component.

--- src/components/courses/Course.jsx
+++ src/components/courses/Course.jsx
@@ -9,22 +9,22 @@ const Course = ({ course, plan }) => {
             </div>
             <div className='border-y mt-4 py-1 border-gray-300 flex items-center justify-between'>
                 <div className="flex flex-col">
-                    <span className='text-emerald-600 text-[17px] font-bold'>{plan.number_of_classes_per_week} per week</span>
+                    <span className='text-amber-600 text-[17px] font-bold'>{plan.number_of_classes_per_week} per week</span>
                     <span className='font-semibold'>Sessions</span>
                 </div>
                 <div className="flex flex-col">
-                    <span className='text-emerald-600 text-[17px] font-bold'>{course.level}</span>
+                    <span className='text-amber-600 text-[17px] font-bold'>{course.level}</span>
                     <span className='font-semibold'>Level</span>
                 </div>
                 <div className="flex flex-col">
-                    <span className='text-emerald-600 text-[17px] font-bold'><span className='me-[1px]'>$</span>{course.fee}</span>
+                    <span className='text-amber-600 text-[17px] font-bold'><span className='me-[1px]'>$</span>{course.fee}</span>
                     <span className='font-semibold'>Price</span>
                 </div>
             </div>
             <p className='text-[rgb(133,128,128)] mt-6'>{course.brief_description}</p>
             <div className="flex justify-between items-end">
                 <Link to={`/courses/${course.slug}/`} state={{ plan }}>
-                    <button className='bg-emerald-600 text-white h-11 grid place-items-center text-sm font-semibold px-5 rounded-[7px] mt-6'>
+                    <button className='bg-amber-600 text-white h-11 grid place-items-center text-sm font-semibold px-5 rounded-[5px] mt-6'>
                         Study Now
                     </button>
                 </Link>
